bd41e695dbf63406ed1e244caefb2906f34a02c3
Ivan Murzin <96486565+AlliumPro@users.noreply.github.com>
2022-05-08 14:23:45 +0500

    Add files via upload

---
+++ Translator bot.py
@@ -0,0 +1,37 @@
+import telebot
+from googletrans import Translator
+token='5305325378:AAHIYkcZbENTP_R444uO-3iSAQqYxldphkU'
+
+bot=telebot.TeleBot(token)
+translator = Translator()
+
+lang=''
+languages={'af': 'afrikaans', 'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy': 'armenian', 'az': 'azerbaijani', 'eu': 'basque', 'be': 'belarusian', 'bn': 'bengali', 'bs': 'bosnian', 'bg': 'bulgarian', 'ca': 'catalan', 'ceb': 'cebuano', 'ny': 'chichewa', 'zh-cn': 'chinese (simplified)', 'zh-tw': 'chinese (traditional)', 'co': 'corsican', 'hr': 'croatian', 'cs': 'czech', 'da': 'danish', 'nl': 'dutch', 'en': 'english', 'eo': 'esperanto', 'et': 'estonian', 'tl': 'filipino', 'fi': 'finnish', 'fr': 'french', 'fy': 'frisian', 'gl': 'galician', 'ka': 'georgian', 'de': 'german', 'el': 'greek', 'gu': 'gujarati', 'ht': 'haitian creole', 'ha': 'hausa', 'haw': 'hawaiian', 'iw': 'hebrew', 'hi': 'hindi', 'hmn': 'hmong', 'hu': 'hungarian', 'is': 'icelandic', 'ig': 'igbo', 'id': 'indonesian', 'ga': 'irish', 'it': 'italian', 'ja': 'japanese', 'jw': 'javanese', 'kn': 'kannada', 'kk': 'kazakh', 'km': 'khmer', 'ko': 'korean', 'ku': 'kurdish (kurmanji)', 'ky': 'kyrgyz', 'lo': 'lao', 'la': 'latin', 'lv': 'latvian', 'lt': 'lithuanian', 'lb': 'luxembourgish', 'mk': 'macedonian', 'mg': 'malagasy', 'ms': 'malay', 'ml': 'malayalam', 'mt': 'maltese', 'mi': 'maori', 'mr': 'marathi', 'mn': 'mongolian', 'my': 'myanmar (burmese)', 'ne': 'nepali', 'no': 'norwegian', 'ps': 'pashto', 'fa': 'persian', 'pl': 'polish', 'pt': 'portuguese', 'pa': 'punjabi', 'ro': 'romanian', 'ru': 'russian', 'sm': 'samoan', 'gd': 'scots gaelic', 'sr': 'serbian', 'st': 'sesotho', 'sn': 'shona', 'sd': 'sindhi', 'si': 'sinhala', 'sk': 'slovak', 'sl': 'slovenian', 'so': 'somali', 'es': 'spanish', 'su': 'sundanese', 'sw': 'swahili', 'sv': 'swedish', 'tg': 'tajik', 'ta': 'tamil', 'te': 'telugu', 'th': 'thai', 'tr': 'turkish', 'uk': 'ukrainian', 'ur': 'urdu', 'uz': 'uzbek', 'vi': 'vietnamese', 'cy': 'welsh', 'xh': 'xhosa', 'yi': 'yiddish', 'yo': 'yoruba', 'zu': 'zulu', 'fil': 'Filipino', 'he': 'Hebrew'}
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == '/help':
+        help=['/help - for help', '/start - to start the translator']
+        for i in range(len(help)):
+            bot.send_message(message.from_user.id, help[i])
+    elif message.text == '/start':
+        bot.send_message(message.from_user.id, 'Enter destination language')
+        bot.register_next_step_handler(message, get_language)
+    else:
+        bot.send_message(message.from_user.id, "I am the Translator Bot. Type '/help' for command list or '/start' to get started.")
+
+def get_language(message):
+    global lang
+    if languages.get(message.text)!=None:
+        lang=str(message.text)
+        bot.send_message(message.from_user.id, 'Enter text you want to translate')
+        bot.register_next_step_handler(message, translate)
+    else:
+        bot.send_message(message.from_user.id,  "Type /start and enter valid language.")
+
+
+def translate(message):
+    global lang
+    result=translator.translate(message.text, dest=lang)
+    bot.send_message(message.from_user.id, result.text)
+
+bot.polling(none_stop=True, interval=0)
